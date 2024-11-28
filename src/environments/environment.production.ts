1cdf819c88aebb783a02cf1d10e44bf4015d2ba4
caique <caique3066@gmail.com>
2024-11-28 07:38:25 +0100

    commit coolify

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+const BACKEND_URL = "//localhost:8080"; //alterar para o nosso dominio depois
+
+export const environment = {
+  BACKEND_URL: BACKEND_URL,
+  API_URL: `${BACKEND_URL}/api`,
+  AUTH0_CLIENT_ID: "b7xqEI0C39EETyGg3LPDTf4dLzNb7TEI",
+  AUTH0_DOMAIN: "https://dev-y5baoume7byicgno.us.auth0.com",
+};
