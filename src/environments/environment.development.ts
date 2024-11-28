abdc8721e63c469b9e0b18fb9a14753de7a71ee5
caique <caique3066@gmail.com>
2024-11-28 13:59:08 +0100

    commit coolify

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ const BACKEND_URL = "https://bike-api.caiquecode.pro";
 export const environment = {
   BACKEND_URL: BACKEND_URL,
   API_URL: `${BACKEND_URL}/api`,
-  AUTH0_CLIENT_ID: "b7xqEI0C39EETyGg3LPDTf4dLzNb7TEI",
+  AUTH0_CLIENT_ID: "YwvccopaYENHZdyucBPn1yJrLoWEZZ02",
   AUTH0_DOMAIN: "https://dev-y5baoume7byicgno.us.auth0.com",
 };
